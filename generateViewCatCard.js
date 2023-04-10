60296b12d910b280333bb0b0d1a52211e4cb299d
MaximProdantsov <maxneto@mail.ru>
2023-04-10 22:04:27 +0300

    не отображался рейтинг при просмотре, теперь работает.

--- generateViewCatCard.js
+++ generateViewCatCard.js
@@ -5,7 +5,7 @@ return `<div class="viewCatCard">
 <h2>Данные о котике</h2>
 <h2>Имя котика: ${cat.name}</h2>
   <img src=${cat.image} alt="Ссылка на изображение">
-  <div>Рейтинг: ${cat.rage || "Рейтинга у котика нет..."}</div>
+  <div>Рейтинг: ${cat.rate || "Рейтинга у котика нет..."}</div>
   <div>Возраст: ${typeof cat.age === "number" ? cat.age : "Возраст не указан"}</div>
   <div>Информация: ${cat.description || "Информации о котике пока нет..."}</div>
 </div>
